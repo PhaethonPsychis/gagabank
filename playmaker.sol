7bd2369191800b1ba5596b087fe75e137ae3b77e
Phaethon Psychis <80890815+PhaethonPsychis@users.noreply.github.com>
2022-02-26 01:15:11 +0200

    Update playmaker.sol

--- playmaker.sol
+++ playmaker.sol
@@ -1,168 +1,70 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.12;
 
+// This contract is a type of aasurance contract designed to operate in the field of Libertarian Social Engineering.
+// to allow for the provision of certain classes of public goods.
+// 
+// It establishes a crowdfunding process with minimal restrictions. 
+// @owner sets a price to sell an upcoming product
+// @owner may or may not set the max number of sponsors this campaign accepts. There is no minimum thershold
+// @owner may or may not set a termination date for this contract
+// The contract receives contributions from sponsors and anounces the events on the network
+// @owner stores the addresses on a databse off chain
+// @owner provides a process to use the list for the purchase of digital goods
 
-//SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
+contract preSale {
 
-import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/utils/math/SafeMath.sol";
-
-
-/**
- * @dev Provides information about the current execution context, including the
- * sender of the transaction and its data. While these are generally available
- * via msg.sender and msg.data, they should not be accessed in such a direct
- * manner, since when dealing with meta-transactions the account sending and
- * paying for execution may not be the actual sender (as far as an application
- * is concerned).
- *
- * This contract is only required for intermediate, library-like contracts.
- */
-abstract contract Context {
-    function _msgSender() internal view virtual returns (address) {
-        return msg.sender;
-    }
-
-    function _msgData() internal view virtual returns (bytes calldata) {
-        return msg.data;
-    }
-}
-
-/**
- * @dev Contract module which provides a basic access control mechanism, where
- * there is an account (an owner) that can be granted exclusive access to
- * specific functions.
- *
- * By default, the owner account will be the one that deploys the contract. This
- * can later be changed with {transferOwnership}.
- *
- * This module is used through inheritance. It will make available the modifier
- * `onlyOwner`, which can be applied to your functions to restrict their use to
- * the owner.
- */
-abstract contract Ownable is Context {
-    address private _owner;
-
-    event OwnershipTransferred(address indexed previousOwner, address indexed newOwner);
-
-    /**
-     * @dev Initializes the contract setting the deployer as the initial owner.
-     */
-    constructor() {
-        _transferOwnership(_msgSender());
-    }
-
-    /**
-     * @dev Returns the address of the current owner.
-     */
-    function owner() public view virtual returns (address) {
-        return _owner;
-    }
-
-    /**
-     * @dev Throws if called by any account other than the owner.
-     */
-    modifier onlyOwner() {
-        require(owner() == _msgSender(), "Ownable: caller is not the owner");
-        _;
-    }
-
-    /**
-     * @dev Leaves the contract without owner. It will not be possible to call
-     * `onlyOwner` functions anymore. Can only be called by the current owner.
-     *
-     * NOTE: Renouncing ownership will leave the contract without an owner,
-     * thereby removing any functionality that is only available to the owner.
-     */
-    function renounceOwnership() public virtual onlyOwner {
-        _transferOwnership(address(0));
-    }
-
-    /**
-     * @dev Transfers ownership of the contract to a new account (`newOwner`).
-     * Can only be called by the current owner.
-     */
-    function transferOwnership(address newOwner) public virtual onlyOwner {
-        require(newOwner != address(0), "Ownable: new owner is the zero address");
-        _transferOwnership(newOwner);
-    }
-
-    /**
-     * @dev Transfers ownership of the contract to a new account (`newOwner`).
-     * Internal function without access restriction.
-     */
-    function _transferOwnership(address newOwner) internal virtual {
-        address oldOwner = _owner;
-        _owner = newOwner;
-        emit OwnershipTransferred(oldOwner, newOwner);
-    }
-}
-
-
-contract preSale is Ownable {
-
-  using SafeMath for uint256;
+  // using SafeMath for uint256;
   // address payable public owner;
-  event Registration(address, uint);
+  
+  address public owner;
+  uint price;
+  string dapp_name;
+  uint maxSponsors;
+
+  event registrationEvent(address sponsor, uint amount, bool success);
+  event RegistrationPrice( address owner, uint price);
 
   constructor() payable {
+      owner = msg.sender;
+      uint price;
+      uint maxSponsors;
   
   }
 
-  receive() external payable {
-    require(msg.value >= 0.1 * (1 ether), "not enough funds");
-    emit Registration(msg.sender, msg.value);
+    modifier onlyOwner() {
+            require(msg.sender == owner, "Not owner");
+            // Underscore is a special character only used inside
+        // a function modifier and it tells Solidity to
+        // execute the rest of the code.
+        _;
+    }
+
+  // @owner can change wallet address  
+  function transferOwnership(address newOwner) public onlyOwner {
+            require(_addr != address(0), "Not valid address");
+      
+      owner = newOwner;
+    }
+
+  // Owner sets the price for the product that this contract will sell
+  function setPrice (uint _price) external onlyOwner{
+      price = _price;
   }
-  // introduce fallback function
-  // TODO allow owner to set msg.value as a parameter
-  fallback () external payable {
-    require(msg.value >= 0.1 * (1 ether), "not enough funds");
+   
+  //@owner set the maximum number of sponsors this preregistration contract accepts
+  function counterSponsors(uint _counterSponsors) external onlyOwner {
+    maxSponsors = _counterSponsors;
+
   }
 
-// Transfer funds operations
-//Owner transfer funds to recipients
-// TODO Introduce mapping to allow owner to register  recipients and amounts
-// The intention of this functio is to allow recipients to initiate the transfer
-  function Payout(address payable _to) public payable onlyOwner {
-        // Call returns a boolean value indicating success or failure.
-        // This is the current recommended method to use.
-    (bool sent,) = _to.call{value: msg.value}("");
-    require(sent, "Failed to send Ether");
-    
+  function update_name(string memory value) public {
+    dapp_name = value;
   }
-//Only Owner set withdraw amount
 
-    function withdraw(uint _amount) external onlyOwner {
-    //    require(owner() == _msgSender(), "Ownable: caller is not the owner");
-        require(_amount < (address(this).balance), "not enough funds");
-        payable(msg.sender).transfer(_amount);
-    }
+  function registration() external payable {
 
-    function getBalance() external view returns (uint) {
-        return address(this).balance;
-    }
-//Only Owner withdraw contract balance using call
-    function withdraw() public payable onlyOwner {
-        (bool sent,) =  (msg.sender).call{value: (address(this).balance)}("");
-       
-    }
-
-    function sendViaCall(address payable _to) public payable onlyOwner {
-        // Call returns a boolean value indicating success or failure.
-        // This is the current recommended method to use.
-        (bool sent, bytes memory data) = _to.call{value: msg.value}("");
-        require(sent, "Failed to send Ether");
-    }
-
-    mapping (address => uint) recipientLedger;
-    // onlyOwner sets amount and recipients in payoutLedger
-
-    function setAmountRecipient(uint _amount, address payable _recipient) internal onlyOwner;
-
-    //recipient initiates payment
-    function payout() public {
-        uint amount = recipientLedger[msg.sender];
-        // Remember to zero the pending refund before
-        // sending to prevent re-entrancy attacks
-        pendingWithdrawals[msg.sender] = 0;
-        payable(msg.sender).transfer(amount);
-    }
+    require(_counterSponsors =< maxSponsors, "Crowdfunding target achieved. Thank you!");
+  }
+  
 }
