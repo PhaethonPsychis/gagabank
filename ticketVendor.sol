f129d507b987108887f5a34fb7ae0b52a3c9fafa
Phaethon Psychis <80890815+PhaethonPsychis@users.noreply.github.com>
2022-03-31 22:12:22 +0300

    Update ticketVendor.sol

--- ticketVendor.sol
+++ ticketVendor.sol
@@ -1,135 +1,28 @@
-//SPDX-License-Identifier: MIT
-pragma solidity ^0.8.13;
+/SPDX-License-Identifier: MIT
+pragma solidity ^0.8.7;
 
 
 //import "@openzeppelin/contracts/access/Ownable.sol";
-import "./ERC20Tickets.sol";
 //import "@openzeppelin/contracts/utils/Context.sol";
+import "https://github.com/PhaethonPsychis/playmaker/blob/main/MetaverseLandsTicket.sol";
 
 
 
-
-abstract contract Context {
-    function _msgSender() internal view virtual returns (address) {
-        return msg.sender;
-    }
-
-    function _msgData() internal view virtual returns (bytes calldata) {
-        return msg.data;
-    }
-}
-
-/**
- * @dev Contract module which provides a basic access control mechanism, where
- * there is an account (an owner) that can be granted exclusive access to
- * specific functions.
- *
- * By default, the owner account will be the one that deploys the contract. This
- * can later be changed with {transferOwnership}.
- *
- * This module is used through inheritance. It will make available the modifier
- * `onlyOwner`, which can be applied to your functions to restrict their use to
- * the owner.
- */
-abstract contract Ownable is Context {
-    address private _owner;
-
-    event OwnershipTransferred(address indexed previousOwner, address indexed newOwner);
-
-    /**
-     * @dev Initializes the contract setting the deployer as the initial owner.
-     */
-    constructor() {
-        _transferOwnership(_msgSender());
-    }
-
-    /**
-     * @dev Returns the address of the current owner.
-     */
-    function owner() public view virtual returns (address) {
-        return _owner;
-    }
-
-    /**
-     * @dev Throws if called by any account other than the owner.
-     */
-    modifier onlyOwner() {
-        require(owner() == _msgSender(), "Ownable: caller is not the owner");
-        _;
-    }
-
-    /**
-     * @dev Leaves the contract without owner. It will not be possible to call
-     * `onlyOwner` functions anymore. Can only be called by the current owner.
-     *
-     * NOTE: Renouncing ownership will leave the contract without an owner,
-     * thereby removing any functionality that is only available to the owner.
-     */
-    //function renounceOwnership() public virtual onlyOwner {
-    //    _transferOwnership(address(0));
-    //}
-
-    /**
-     * @dev Transfers ownership of the contract to a new account (`newOwner`).
-     * Can only be called by the current owner.
-     */
-    function transferOwnership(address newOwner) public virtual onlyOwner {
-        require(newOwner != address(0), "Ownable: new owner is the zero address");
-        _transferOwnership(newOwner);
-    }
-
-    /**
-     * @dev Transfers ownership of the contract to a new account (`newOwner`).
-     * Internal function without access restriction.
-     */
-    function _transferOwnership(address newOwner) internal virtual {
-        address oldOwner = _owner;
-        _owner = newOwner;
-        emit OwnershipTransferred(oldOwner, newOwner);
-    }
-}
-
-/**
- * @dev Provides information about the current execution context, including the
- * sender of the transaction and its data. While these are generally available
- * via msg.sender and msg.data, they should not be accessed in such a direct
- * manner, since when dealing with meta-transactions the account sending and
- * paying for execution may not be the actual sender (as far as an application
- * is concerned).
- *
- * This contract is only required for intermediate, library-like contracts.
- */
-
-
-/**
- * @dev Contract module which provides a basic access control mechanism, where
- * there is an account (an owner) that can be granted exclusive access to
- * specific functions.
- *
- * By default, the owner account will be the one that deploys the contract. This
- * can later be changed with {transferOwnership}.
- *
- * This module is used through inheritance. It will make available the modifier
- * `onlyOwner`, which can be applied to your functions to restrict their use to
- * the owner.
- */
-
-
-contract ticketVendor is Ownable {
+contract ticketVendor {
 
   // Ticket price variable;
-  uint public price;
+  //uint public price;
 
  // Event that log buy operation
   //event purchaseTickets(address buyer, uint256 amountOfETH, uint256 amountOfTokens);
   //event SellTokens(address seller, uint256 amountOfTokens, uint256 amountOfETH);
 
-  ERC20Tickets public ERC20Tickets_;
+  MetaverseLandsTicket public MetaverseLandsTicket_;
 
   constructor(address tokenAddress) {
 
     owner = payable(msg.sender);
-    ERC20Tickets_ = ERC20Tickets(tokenAddress);
+    MetaverseLandsTicket_ = MetaverseLandsTicket(tokenAddress);
   }
   // Allow owner to set the price for the tickets
   function setTicketsPrice(uint price) public {
@@ -141,7 +34,7 @@ contract ticketVendor is Ownable {
   // Allow anyone to purchase tickets
   //Buyer sets the number of tickets to buy
     function purchaseTickets(uint tickets) public payable returns (uint256 Tickets) {
-        require(msg.value >= amount * _price, "You must pay at least 0.01 ETH per ticket");
+        require(msg.value >= amount * price, "You must pay at least 0.01 ETH per ticket");
 
         // check if the ticketVendor Contract has enough amount of tickets for the transaction
         uint256 ticketBalanceVendor = ERC2OTickets.balanceOf(address(this));
@@ -167,7 +60,7 @@ contract ticketVendor is Ownable {
         require(msg.value >= 0.1 * (1 ether), "not enough funds");
   }
 
-  //@dev Allow Only owner of the contract ro withdraw funds
+  //@dev Allow Only owner of the contract to withdraw funds
   //todo address msg.sender must be payable
     function withdraw() public payable {
        uint balance = _balances[msg.sender];
