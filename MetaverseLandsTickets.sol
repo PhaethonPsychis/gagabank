4c522c0c393c403ea64f9bc99727ea37a9a8fd2d
Phaethon Psychis <80890815+PhaethonPsychis@users.noreply.github.com>
2022-03-31 20:39:28 +0300

    Update MetaverseLandsTickets.sol

--- MetaverseLandsTickets.sol
+++ MetaverseLandsTickets.sol
@@ -5,7 +5,7 @@ pragma solidity ^0.8.7;
 // import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/IERC20Metadata.sol";
-//import "@openzeppelin/contracts/utils/Context.sol";
+import "@openzeppelin/contracts/utils/Context.sol";
 //------------------------------------------------------------------------------------------------------------------------
 //MLI Metaverse Lands Initiative
 //------------------------------------------------------------------------------------------------------------------------
